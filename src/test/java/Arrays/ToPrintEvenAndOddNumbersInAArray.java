7613dd13439f05f8037e724cd2090e684537748e
ShaikHassain <hassainshaik347@gmail.com>
2024-10-10 20:27:23 +0530

    arraysassignment commit

---
+++ src/test/java/Arrays/ToPrintEvenAndOddNumbersInAArray.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class ToPrintEvenAndOddNumbersInAArray {
+    public static void main(String[] args) {
+        int a[]={100,1,5654,7843,2365,213,453,236,456,74};
+        for (int i=0;i<a.length;i++)
+        {
+            int reqnum=0;
+            if (a[i]%2==0)
+            {
+                System.out.println("even number:"+a[i]);
+            }
+            else
+            {
+                System.out.println("Odd number: "+a[i]);
+            }
+        }
+    }
+}
