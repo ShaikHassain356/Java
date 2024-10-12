9d6acab8c4e63a12d1db7f99cdc82151ff01a0cc
ShaikHassain <hassainshaik347@gmail.com>
2024-10-12 20:11:03 +0530

    arraysassignment commit

---
+++ src/test/java/Arrays/ArraySortingInAscendingOrder.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArraySortingInAscendingOrder {
+    public static void main(String[] args) {
+        int a[]= {1,6,1,300,200,100,50,60,900,600,69,78};
+        //without for loop reading all the values in array using predefined class array and method tostring()
+        System.out.println("Array before sorting: "+ Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println("Array after sorting: "+Arrays.toString(a));
+    }
+}
---
+++ src/test/java/Arrays/SearchingAnElementInAnArray.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class SearchingAnElementInAnArray {
+    public static void main(String[] args) {
+        Object a[][]={{100,10.25,"Hello"},
+                      {'A',789,"Hi"},
+                      {'R',"Iam there",63.0235}};
+        Object reqelement=1;
+        boolean elementfound = false;
+        for (int  i=0;i<a.length;i++)
+        {
+            for (int j=0;j<a[1].length;j++)
+            {
+                if (a[i][j].equals(reqelement)) // we should not use == here for primitive data types
+                {
+                    System.out.println("Required element is found at: ["+i+"]["+j+"]");
+                    elementfound =true;
+                    break;
+
+                }
+
+            }
+
+        }
+        if (elementfound==false)
+        {
+            System.out.println("element not found");
+        }
+
+
+    }
+}
