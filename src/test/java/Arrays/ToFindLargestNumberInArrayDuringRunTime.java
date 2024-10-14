a4406c5a6db99486912db1d41aed3346c0ae3e61
ShaikHassain <hassainshaik347@gmail.com>
2024-10-14 20:31:59 +0530

    oct14_3 commit

---
+++ src/test/java/Arrays/ToFindLargestNumberInArrayDuringRunTime.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ToFindLargestNumberInArrayDuringRunTime {
+    public static void main(String[] args) {
+        int a[]=new int[5];
+        Scanner sc=new Scanner(System.in);
+        for (int i=0;i<a.length;i++)
+        {
+            System.out.println("Enter the number at the position"+i+":");
+            a[i]= sc.nextInt();
+        }
+        int smallest =a[0];
+        for (int i=1;i<a.length;i++)
+        {
+            if (a[i]<smallest)
+            {
+                smallest=a[i];
+            }
+        }
+        System.out.println("smallest number in the provided array is: "+smallest);
+    }
+}
