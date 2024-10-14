8e32323f1584e3c6d97aeb990ede2e2880985256
ShaikHassain <hassainshaik347@gmail.com>
2024-10-14 20:11:41 +0530

    oct14 commit

---
+++ src/test/java/Arrays/ReadingIntegerArrayElementsAtRunTime.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ReadingIntegerArrayElementsAtRunTime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer a[]=new Integer[6];
+        for (int i=0;i<a.length;i++)
+        {
+            System.out.println("Enter a number for the position "+i+":");
+            a[i]= sc.nextInt();
+        }
+        System.out.println("Array of elements: "+ Arrays.toString(a));
+        System.out.println(a[5]);
+        Arrays.sort(a);
+        System.out.println("After sorting in ascending order:"+Arrays.toString(a));
+        Arrays.sort(a,Collections.reverseOrder());
+        System.out.println("After sorting in Descending order: "+Arrays.toString(a));
+    }
+}
---
+++ src/test/java/Arrays/ToReverseANumberGivingNumAtRunTime.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ToReverseANumberGivingNumAtRunTime {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc=new Scanner(System.in);
+        int reqnum = sc.nextInt();
+        int tempnum =0;
+        while (reqnum!=0)
+        {
+           int j= reqnum%10;
+           tempnum =tempnum*10+j;
+           reqnum= reqnum/10;
+        }
+        System.out.println("After reversing a number is:"+tempnum);
+    }
+}
