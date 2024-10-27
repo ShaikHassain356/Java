bdf0371ce6dea65a9f72c26a13dd5e21143c5e48
ShaikHassain <hassainshaik347@gmail.com>
2024-10-27 15:53:41 +0530

    oct27_2 commit

---
+++ src/test/java/OOPsConcepts/AssigningDataUsingMethods/Student.java
@@ -0,0 +1,32 @@
+package OOPsConcepts.AssigningDataUsingMethods;
+
+public class Student {
+
+
+    int stuid;
+    String sname;
+    char sgrad;
+
+    void printstudentdata()
+    {
+        System.out.println(stuid+"  "+sname+"  "+sgrad);
+    }
+
+    void setstudentdata(int id,String name, char gr)
+    {
+        stuid=id;
+        sname=name;
+        sgrad=gr;
+    }
+
+    //Constructor
+    //Constructor name should always same as class name
+    //assigning data to the class variables
+    //mostly we use constructor to assign the data because at the time of object creation constructor automatically invoked
+    Student(int id, String name, char gr)
+    {
+        stuid=id;
+        sname=name;
+        sgrad=gr;
+    }
+}
